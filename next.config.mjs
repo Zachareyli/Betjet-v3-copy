74172250a7149abac066bc90ebb80d8d13739c62
Zachary Nelson <zaclayton44@gmail.com>
2024-08-04 22:56:24 +0200

    update

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['cdn.play-gaming.com','www.montecasino.co.za', 'www.betgames.tv','https://central.bet.co.za'],
-    },
-  };
-  
-  export default nextConfig;
-  
