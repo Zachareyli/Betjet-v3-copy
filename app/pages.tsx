09d2755c4fdc7d1a8e06969b06a0a7aca6a62c55
Zachary Nelson <zaclayton44@gmail.com>
2024-08-05 13:53:04 +0200

    upd
    upd

---
+++ app/pages.tsx
@@ -0,0 +1,31 @@
+
+import "./globals.css";
+// import Navbar from "@/components/navbar";
+import TailwindNavbar from "@/components/navbarTailwind";
+import Banner from "@/components/bannerTailwind";
+import ProductList from "@/components/products";
+import PromoSection from "@/components/promoSection";
+import ContactUs from "@/components/contactUs";
+import HeaderSection from "@/components/nextHeader";
+
+
+
+export default function Home() {
+  return (
+      <body>
+        <meta name="keywords" content="Skyward, Bet.co.za, Evolution Gaming, Bet Games, online casino, new games" />
+        <meta property="og:title" content="Skyward Has Landed at Bet.co.za!" />
+        <meta property="og:description" content="Experience the thrill of Skyward, the innovative new casino game, now available at Bet.co.za." />
+        <meta property="og:url" content="https://www.bet.co.za/" /> <meta property="og:image" content="https://www.bet.co.za/img/26436/og-image.png" /> <meta property="twitter:title" content="Skyward Has Landed at Bet.co.za!" />
+        <meta property="twitter:description" content="Experience the thrill of Skyward, the innovative new casino game, now available at Bet.co.za." />
+        <meta property="twitter:card" content="https://www.bet.co.za/img/26436/og-image.png" />
+        <HeaderSection />
+        <Banner />
+        <TailwindNavbar />
+        {/* <Navbar/> */}
+        <PromoSection />
+        <ProductList />
+        <ContactUs />
+      </body>    
+  );
+}
